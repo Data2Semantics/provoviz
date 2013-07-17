ab789dd21ee236bc9f1e33421c179c2359270bbb
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-07-17 10:18:00 +0200

    Added option for static url path

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,7 +4,12 @@ import os
 
 TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
-app = Flask(__name__, template_folder = TEMPLATE_FOLDER)
+if STATIC_URL_PATH :
+    app = Flask(__name__, template_folder = TEMPLATE_FOLDER, static_url_path = STATIC_URL_PATH)
+else :
+    app = Flask(__name__, template_folder = TEMPLATE_FOLDER)
+
+
 
 app.config.from_object('config')
 
