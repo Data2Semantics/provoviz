5dcfe20984822430829695fb468119b7d78422cd
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-07-17 10:19:15 +0200

    Rollback

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,10 +4,8 @@ import os
 
 TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
-if STATIC_URL_PATH :
-    app = Flask(__name__, template_folder = TEMPLATE_FOLDER, static_url_path = STATIC_URL_PATH)
-else :
-    app = Flask(__name__, template_folder = TEMPLATE_FOLDER)
+
+app = Flask(__name__, template_folder = TEMPLATE_FOLDER)
 
 
 
