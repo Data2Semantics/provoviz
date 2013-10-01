ff43b2d66efc482d9e7d8727626b8df6312a5700
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-10-01 19:52:59 +0200

    Service works, now over to rendering the results

--- src/app/views.py
+++ src/app/views.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
 
-from flask import render_template, g, request, jsonify
+from flask import render_template, g, request, jsonify, make_response
 import util.sparql as s
 from bs4 import BeautifulSoup
 import requests
 import os
 import os.path
 from datetime import datetime
+import json 
 
 from app import app
+
+DEFAULT_SPARQL_ENDPOINT_URL = "http://semweb.cs.vu.nl:8080/openrdf-sesame/repositories/goldendemo"
+DEFAULT_RDF_DATA_UPLOAD_URL = "http://semweb.cs.vu.nl:8080/openrdf-sesame/repositories/goldendemo/statements"
 
 @app.route('/')
 def index():
@@ -69,8 +73,55 @@ def diagram():
 
 @app.route('/service', methods=['POST'])
 def service():
-    pass
+    prov_data = request.form['data']
+    graph_uri = request.form['graph_uri']
+    context = "<{}>".format(graph_uri)
+    
+    
+    headers =  {'content-type':'text/turtle;charset=UTF-8'}
+    params = {'context': context}
 
+    r = requests.put(DEFAULT_RDF_DATA_UPLOAD_URL,
+                     data = prov_data,
+                     params = params,
+                     headers = headers)
+    
+    if r.ok :
+        ## It seems we're good to go!
+        activities = s.get_activities(graph_uri, DEFAULT_SPARQL_ENDPOINT_URL)
+        
+        response = []
+        for a in activities:
+            activity_uri = a['id']
+            activity_id = a['text']
+            
+            try:
+                graph, width, types, diameter = s.build_activity_graph(activity_uri, activity_id, graph_uri, DEFAULT_SPARQL_ENDPOINT_URL)
+            except Exception as e:
+                app.logger.debug(e)
+                app.logger.debug("Continuing as if nothing happened...")
+                continue
+                
+            activity = {}
+            activity['id'] = activity_uri
+            activity['text'] = activity_id
+            activity['graph'] = graph
+            activity['width'] = width
+            activity['types'] = types
+            activity['diameter'] = diameter
+        
+            response.append(activity)
+        
+        response = json.dumps(response)
+        return render_template('activities_service.html', response=response)
+        
+    else :
+        return make_response(r.content, 500)
+    
+    
+    
+    
+    
 
 
 
