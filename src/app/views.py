ddabeffa36b0757488f50f6b33cfe24973cf6a8d
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2013-11-12 13:44:25 +0100

    Removed CORS stuff

--- src/app/views.py
+++ src/app/views.py
@@ -19,46 +19,6 @@ from app import app
 DEFAULT_SPARQL_ENDPOINT_URL = "http://semweb.cs.vu.nl:8080/openrdf-sesame/repositories/goldendemo"
 DEFAULT_RDF_DATA_UPLOAD_URL = "http://semweb.cs.vu.nl:8080/openrdf-sesame/repositories/goldendemo/statements"
 
-def crossdomain(origin=None, methods=None, headers=None,
-				max_age=21600, attach_to_all=True,
-				automatic_options=True):
-	if methods is not None:
-		methods = ', '.join(sorted(x.upper() for x in methods))
-	if headers is not None and not isinstance(headers, basestring):
-		headers = ', '.join(x.upper() for x in headers)
-	if not isinstance(origin, basestring):
-		origin = ', '.join(origin)
-	if isinstance(max_age, timedelta):
-		max_age = max_age.total_seconds()
-
-	def get_methods():
-		if methods is not None:
-			return methods
-
-		options_resp = current_app.make_default_options_response()
-		return options_resp.headers['allow']
-
-	def decorator(f):
-		def wrapped_function(*args, **kwargs):
-			if automatic_options and request.method == 'OPTIONS':
-				resp = current_app.make_default_options_response()
-			else:
-				resp = make_response(f(*args, **kwargs))
-			if not attach_to_all and request.method != 'OPTIONS':
-				return resp
-
-			h = resp.headers
-
-			h['Access-Control-Allow-Origin'] = origin
-			h['Access-Control-Allow-Methods'] = get_methods()
-			h['Access-Control-Max-Age'] = str(max_age)
-			if headers is not None:
-				h['Access-Control-Allow-Headers'] = headers
-			return resp
-
-		f.provide_automatic_options = False
-		return update_wrapper(wrapped_function, f)
-	return decorator
 
 
 @app.route('/')
@@ -122,7 +82,6 @@ def activities():
 
 
 @app.route('/service', methods=['POST','OPTIONS'])
-@crossdomain(origin='*')
 def service():
 	prov_data = request.form['data']
 	graph_uri = request.form['graph_uri']
