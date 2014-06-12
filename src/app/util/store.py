2ece9bfbb48091e135b606c5f2591f415f1d10dc
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-06-12 15:09:32 +0200

    New store abstraction layer based on RDFLib

---
+++ src/app/util/store.py
@@ -0,0 +1,56 @@
+from rdflib.plugins.sparql.results.jsonresults import JSONResultParser
+from rdflib import Graph
+from StringIO import StringIO
+from app import app, socketio
+from flask import url_for
+import requests
+
+class Store(object):
+    
+    # Initialize the store, either with data or with endpoint (but not both)
+    def __init__(self, data=None, endpoint=None, auth=None):
+        if data and not endpoint:
+            emit("Loading PROV data in Turtle format")
+            
+            self.remote = False
+            try :
+                self.graph = Graph()  
+                app.logger.debug('Loading PROV-O definitions from {}'.format(url_for('.static',filename='prov-o.ttl',_external=True)))           
+                self.graph.parse(url_for('.static',filename='prov-o.ttl',_external=True),format="turtle")      
+                app.logger.debug('Loading data...')
+                self.graph.parse(data=data,format="turtle")
+            except Exception as e:
+                emit("Store could not load data (not Turtle?)")
+                
+                raise e
+        
+        elif endpoint and not data:
+            emit("Setting SPARQL connection parameters")
+            
+            self.remote = True
+            self.session = requests.Session()
+            self.session.headers.update({'Accept':'application/sparql-results+json'})
+            
+            if auth:
+                emit("Setting basic HTTP authentication parameters for endpoint {}".format(endpoint))
+                self.session.auth = auth
+            
+            self.endpoint = endpoint
+        else:
+            print "Whoops: cannot give me both data and an endpoint!"
+             
+    def query(self,q):
+        if self.remote :
+            json_results = self.session.get(self.endpoint, params={'query':q})
+            json_parser = JSONResultParser()
+            return json_parser.parse(StringIO(json_results.text))
+        else :
+            return self.graph.query(q)
+            
+
+
+
+def emit(message):
+    socketio.emit('message',
+                  {'data': message },
+                  namespace='/log')
