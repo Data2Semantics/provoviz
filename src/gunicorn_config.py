8b2397a13d26d4795b395bd4ad90d3b063162b20
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-09-24 13:34:56 +0200

    No longer necessary in provomatic

--- src/gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-workers = 1
-worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
-bind = '0.0.0.0:5000'
-pidfile = '/tmp/gunicorn.pid'
-debug = True
-loglevel = 'debug'
-errorlog = '/tmp/gunicorn_error.log'
-accesslog = '/tmp/gunicorn_access.log'
-daemon = False
--- src/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from app import app, socketio
-
-if __name__ == '__main__':
-    socketio.run(app)
-
-#app.run(host='0.0.0.0',port=5000)
