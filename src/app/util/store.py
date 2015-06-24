6e0723c290815daef391b64e5b9efb6b847b70c0
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-06-24 13:44:22 +0200

    More flexible when data and endpoint are both provided (will ignore endpoint)

--- src/app/util/store.py
+++ src/app/util/store.py
@@ -6,46 +6,48 @@ from flask import url_for
 import requests
 
 class Store(object):
-    
+
     # Initialize the store, either with data or with endpoint (but not both)
     def __init__(self, data=None, data_format='turtle', endpoint=None, auth=None):
-        if data and not endpoint:
+        if data:
             emit("Loading PROV data in Turtle format")
-            
+            if endpoint:
+                app.logger.warning("You provided both data and an endpoint... the endpoint will be ignored.")
+
             self.remote = False
             try :
-                self.graph = Graph()  
-                app.logger.debug('Loading PROV-O definitions from {}'.format(url_for('.static',filename='prov-o.ttl',_external=True)))           
-                self.graph.parse(url_for('.static',filename='prov-o.ttl',_external=True),format='turtle')      
-                
-                
+                self.graph = Graph()
+                app.logger.debug('Loading PROV-O definitions from {}'.format(url_for('.static',filename='prov-o.ttl',_external=True)))
+                self.graph.parse(url_for('.static',filename='prov-o.ttl',_external=True),format='turtle')
+
+
                 if data.lower().startswith('http'):
                     app.logger.debug('Loading data from URL: {}'.format(data))
                     self.graph.parse(location=data,format=data_format)
                 else :
                     app.logger.debug('Loading data...')
                     self.graph.parse(data=data,format=data_format)
-                    
+
             except Exception as e:
                 emit("Store could not load data (not Turtle?)")
-                
+
                 raise e
-        
-        elif endpoint and not data:
+
+        elif endpoint:
             emit("Setting SPARQL connection parameters")
-            
+
             self.remote = True
             self.session = requests.Session()
             self.session.headers.update({'Accept':'application/sparql-results+json'})
-            
+
             if auth:
                 emit("Setting basic HTTP authentication parameters for endpoint {}".format(endpoint))
                 self.session.auth = auth
-            
+
             self.endpoint = endpoint
         else:
-            print "Whoops: cannot give me both data and an endpoint!"
-             
+            app.logger.error("Whoops: cannot give me no data nor an endpoint!")
+
     def query(self,q):
         if self.remote :
             try :
@@ -60,7 +62,7 @@ class Store(object):
         else :
             app.logger.debug("Directly querying internal store")
             return self.graph.query(q)
-            
+
 
 
 
