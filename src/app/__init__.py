35dc26dc49e0b91a8bdc9eff45fcc49f16b24373
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2016-06-16 13:39:43 +0200

    Refactoring... added logging import for INFO level.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 from flask import Flask
 import os
+import logging
 
 from flask.ext.socketio import SocketIO, emit
 
 TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 
-
 app = Flask(__name__, template_folder = TEMPLATE_FOLDER, static_folder = STATIC_FOLDER)
 
-
-
 socketio = SocketIO(app)
-
 
 app.config.from_object('config')
 
 app.debug = True
-app.logger.setLevel(20)
+app.logger.setLevel(logging.INFO)
 
 import views
